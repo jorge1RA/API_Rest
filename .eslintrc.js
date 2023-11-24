27f69d7fcb0db1ef52740d4cd21adeb368e21c69
jorge <jromari069@g.educaand.es>
2023-11-24 23:12:20 +0100

    Quitamos código innecesario

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  // Configuración general del entorno donde se ejecuta el código
+  env: {
+    browser: true, // Este código se ejecuta en un navegador
+    es2021: true, // Utiliza características de ECMAScript 2021
+  },
+
+  // Extensiones de configuración utilizadas
+  extends: [
+    "eslint:recommended", // Configuración recomendada por ESLint
+    "plugin:react/recommended", // Reglas recomendadas para React
+  ],
+
+  // Reglas específicas para ciertos archivos o directorios
+  overrides: [
+    {
+      env: {
+        node: true, // Para archivos con nombres ".eslintrc.js" o ".eslintrc.cjs"
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script", // El código se interpreta como un script
+      },
+    },
+  ],
+
+  // Opciones del parser (analizador de código)
+  parserOptions: {
+    ecmaVersion: "latest", // Utiliza la versión más reciente de ECMAScript
+  },
+
+  // Plugins utilizados (como el plugin de ESLint para React)
+  plugins: ["react"],
+
+  // Reglas específicas de ESLint para este proyecto (aún no se han configurado)
+  rules: {
+    // Aquí puedes personalizar y agregar reglas específicas según tus necesidades
+  },
+};
